1981ae264e9ff73ac03908718e3a50e2a06ff655
kcui23 <kcui@foxmail.com>
2023-10-17 04:16:35 -0500

    add 寻找放量涨停

--- volsurge_limitup.py
+++ volsurge_limitup.py
@@ -3,6 +3,44 @@ import VolumeSpikes as vs
 import wave_price_change as wpc
 import high_low_xuejie_zuhui as hl
 
+def find_volsurge_limitup_days(csv_file_name):
+    # 读取CSV文件的股票数据
+    df = pd.read_csv(csv_file_name)
+
+    # 计算平均成交量，采用过去30天的历史数据
+    df['average_volume'] = df['S_DQ_VOLUME'].rolling(window=30).mean()
+    # print(df['average_volume'])
+
+    # 初始化结果列表
+    volsurge_limitup_days = []
+
+    # 将日期列转换为从1开始的数组
+    date_array = list(range(1, len(df) + 1))
+    df['date_array'] = date_array
+
+    # 查找放量涨停日
+    # 需满足三个条件（不确定）：
+    # 1.当日成交量明显高于平均成交量，设置为了1.5倍
+    # 2.当日的涨跌幅达到或超过10%
+    # 3.当日最高价等于涨停价，设置成了大于等于前一交易日的收盘价乘以1.1
+    for index, row in df.iterrows():
+        if row[ 'S_DQ_VOLUME'] > 1.5 * row['average_volume'] \
+                and ((row['S_DQ_CLOSE'] - row['S_DQ_OPEN']) / row['S_DQ_OPEN']) * 100 >= 10 \
+                and row['S_DQ_HIGH'] >= df.at[index - 1, 'S_DQ_CLOSE'] * 1.1:
+            volsurge_limitup_days.append(row['date_array'])
+
+    # 删除日期数组列
+    df.drop('date_array', axis=1, inplace=True)
+
+    # 合并短时间内多个放量涨停
+    volsurge_limitup_days_merged = [volsurge_limitup_days[0]]
+    for i in range(1, len(volsurge_limitup_days)):
+        if volsurge_limitup_days[i] != volsurge_limitup_days[i - 1] + 1:
+            volsurge_limitup_days_merged.append(volsurge_limitup_days[i]) #即将连续子串合并为该子串的第一个元素
+
+
+    return volsurge_limitup_days_merged
+
 
 def cal_wave_info(filename, vol_days_index):
     """
@@ -26,7 +64,7 @@ def cal_wave_info(filename, vol_days_index):
     
     
     df = hl.df_init(filename)
-    vol_days = df['TRADE_DAYS'].values[vol_days_index]
+    vol_days = df['TRADE_DT'].values[vol_days_index]
     waves = wpc.wave_identify(filename)
     position = []
     for day in vol_days:
@@ -55,3 +93,18 @@ def cal_wave_info(filename, vol_days_index):
     wave_type = [x[-4:] for x in wave_type]
     result = pd.DataFrame({'date': vol_days, 'wave_type': wave_type,
                         'price_change': price_change, 'price_change_rate': price_change_rate})
+    
+    return result
+
+if __name__ == '__main__':
+    # 调用函数并读取数据
+    csv_file_path = '000001.SZ.csv'
+    volsurge_limitup_days = find_volsurge_limitup_days(csv_file_path)
+    cals = cal_wave_info(csv_file_path, volsurge_limitup_days)
+    # print(volsurge_limitup_days)
+
+    # 输出结果
+    print("放量涨停日列表：")
+    print(volsurge_limitup_days)
+    print("波段信息：")
+    print(cals)
