51586a4662e8c9d5cf0f7b27a874b5cdf4a0f7ab
kcui23 <kcui@foxmail.com>
2023-10-21 00:22:12 -0500

    add general_eda.py - lightweight histogram and boxplot display of a data column

---
+++ general_eda.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def hist_plot(df, data_col, hue='', quantile=1, absolute=False):
+    """
+    This function plots a histogram of the data_col in the df dataframe.
+    The hue parameter is used to color the histogram based on the hue column.
+    The quantile parameter is used to filter the data_col column based on the quantile value.
+    The absolute parameter is used to take the absolute value of the data_col column.
+    Bonus: if the hue column has only {'rise', 'fall'}, the histogram is colored red and green.
+    
+    Example:
+    --------
+    >>> hist_plot(df, 'price', 'wave_type', quantile=0.95, absolute=True)
+    """
+    sns.set_style("darkgrid")
+
+    quantiled_df = df[(df[data_col] < df[data_col].quantile(quantile)) & (df[data_col] > df[data_col].quantile(1-quantile))].copy()
+    if absolute:
+        quantiled_df[data_col] = quantiled_df[data_col].abs()
+
+    if hue == '':
+        sns.histplot(data=quantiled_df, x=data_col, kde=True)
+        plt.show()
+        return
+    if set(df[hue]) == {'rise', 'fall'}:
+        sns.histplot(data=quantiled_df, x=data_col, hue=hue, kde=True, palette=['red', 'green'])
+    else:
+        sns.histplot(data=quantiled_df, x=data_col, hue=hue, kde=True)
+    plt.show()
+    return
+
+
+def box_plot(df, y, x='', quantile=1, absolute=False):
+    """
+    This function plots a boxplot of the data_col in the df dataframe.
+    The hue parameter is used to color the boxplot based on the hue column.
+    The quantile parameter is used to filter the data_col column based on the quantile value.
+    The absolute parameter is used to take the absolute value of the data_col column.
+    Bonus: if the hue column has only {'rise', 'fall'}, the boxplot is colored red and green.
+    
+    Example:
+    --------
+    >>> box_plot(df, 'price', 'wave_type', quantile=0.95, absolute=True)
+    """
+    sns.set_style("darkgrid")
+
+    quantiled_df = df[(df[y] < df[y].quantile(quantile)) & (df[y] > df[y].quantile(1-quantile))].copy()
+    if absolute:
+        quantiled_df[y] = quantiled_df[y].abs()
+
+    if x == '':
+        ax = sns.boxplot(data=quantiled_df, x=y, width=0.6)
+        ax.set_title(y, fontsize=14)
+        ax.set_xlabel('')
+        plt.xticks(fontsize=14)
+        plt.yticks(fontsize=14)
+        plt.show()
+        return
+
+    plt.figure(figsize=(4, 7))
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    if set(df[x]) == {'rise', 'fall'}:
+        ax = sns.boxplot(data=quantiled_df, x=x, y=y, palette=["#EF5C65", "#68AB6A"])
+        ax.set_title(y, fontsize=16, loc='left')
+        ax.set_xlabel('')
+        ax.set_ylabel('')
+    else:
+        sns.boxplot(data=quantiled_df, x=x, y=y)
+    plt.show()
+    return
