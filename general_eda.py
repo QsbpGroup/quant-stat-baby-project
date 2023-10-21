8a316086aad01847893d547c64aa8c5dd32ddfb3
kcui23 <kcui@foxmail.com>
2023-10-21 00:46:54 -0500

    Beautify figures

--- general_eda.py
+++ general_eda.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def hist_plot(df, data_col, hue='', quantile=1, absolute=False):
+def hist_plot(df, data_col, hue='', bins=40, quantile=1, absolute=False):
     """
     This function plots a histogram of the data_col in the df dataframe.
     The hue parameter is used to color the histogram based on the hue column.
@@ -20,14 +20,15 @@ def hist_plot(df, data_col, hue='', quantile=1, absolute=False):
     if absolute:
         quantiled_df[data_col] = quantiled_df[data_col].abs()
 
+    plt.figure(figsize=(7, 5))
     if hue == '':
-        sns.histplot(data=quantiled_df, x=data_col, kde=True)
+        sns.histplot(data=quantiled_df, x=data_col, bins=bins, kde=True)
         plt.show()
         return
     if set(df[hue]) == {'rise', 'fall'}:
-        sns.histplot(data=quantiled_df, x=data_col, hue=hue, kde=True, palette=['red', 'green'])
+        sns.histplot(data=quantiled_df, x=data_col, hue=hue, bins=bins, kde=True, palette=['red', 'green'])
     else:
-        sns.histplot(data=quantiled_df, x=data_col, hue=hue, kde=True)
+        sns.histplot(data=quantiled_df, x=data_col, hue=hue, bins=bins, kde=True)
     plt.show()
     return
 
@@ -63,11 +64,15 @@ def box_plot(df, y, x='', quantile=1, absolute=False):
     plt.xticks(fontsize=14)
     plt.yticks(fontsize=14)
     if set(df[x]) == {'rise', 'fall'}:
-        ax = sns.boxplot(data=quantiled_df, x=x, y=y, palette=["#EF5C65", "#68AB6A"])
+        ax = sns.boxplot(data=quantiled_df, x=x, y=y, palette=["#fd5a45", "#3bc66d"])
         ax.set_title(y, fontsize=16, loc='left')
         ax.set_xlabel('')
         ax.set_ylabel('')
     else:
         sns.boxplot(data=quantiled_df, x=x, y=y)
+    for i in range(len(quantiled_df.groupby(x))):
+        plt.text(1-i, quantiled_df.groupby(x)[y].median()[i], 
+                round(quantiled_df.groupby(x)[y].median()[i], 2), 
+                ha='center', va='bottom', fontsize=14)
     plt.show()
     return
